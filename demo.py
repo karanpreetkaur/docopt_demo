8f091fa0c31ba509d1ca7d06096c969f161cafc8
Karanpreet Kaur <karanpreet088@gmail.com>
2021-11-19 00:31:35 -0800

    submit exercise-7

---
+++ demo.py
@@ -0,0 +1,18 @@
+# author: Tiffany Timbers
+# date: 2020-01-15
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> [<arg_new>] --arg2=<arg2> [--arg3=<arg3>]
+
+Options:
+<arg>             Takes any value (this is a required positional argument)
+[<arg_new>]       Takes any value (this is a optional positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
+print(opt['<arg_new>'])
